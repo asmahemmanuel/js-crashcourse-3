82d3db87d26bae8004192017cd8d8848e6b61426
AbilityMensah <abilitymensah008@gmail.com>
2022-03-22 07:41:39 -0700

    react component

--- code-along/src/App.js
+++ code-along/src/App.js
@@ -2,27 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import writers from "./writers"
+import { ProfileCard } from './profileCard';
 
 function App() {
   return (
     <div>
       <h1>Writer Profiles</h1>
-      <div className="container">
-        {writers.map((writer) => (
-      <div className="card">
-      <img src={`images/${writer.avatar}.png`}
-           width="300px" 
-           height="300px" 
-           alt="" 
-           />
-      <div className="textGroup">
-        <h3>{writer.name}</h3> 
-        <p>{writer.email}</p> 
-        <p>{writer.phone}</p>
-          </div>
+     <div className="container">
+       {writers.map((writer) => (
+         <ProfileCard writer={writer} />
+       ))} 
+    
         </div>
-        ))}
-      </div>
     </div>
   );
 }
