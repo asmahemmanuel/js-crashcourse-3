1231c2e068d5fad3c3974979bef4841ed2409882
AbilityMensah <abilitymensah008@gmail.com>
2022-03-24 06:50:02 -0700

    React Component 4

--- code-along/src/App.js
+++ code-along/src/App.js
@@ -1,35 +1,70 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import writers from "./writers"
-import { ProfileCard } from './profileCard';
+// import writers from "./writers"
+import ProfileCard from './ProfileCard';
 import { useEffect, useState } from 'react';
+import writers from './writers';
+// import writers from './writers';
 
 function App() {
-  
-const [writers, setWriters] = useState([]);
+const [data, setData] = useState({
+  writers: [],
+  loading: false,
+});
 
-useEffect(() => {
-  const getWriters = async () => {
-    const response = await fetch("/writers.json");
-    const data = await response.json();
-    setWriters(data);
-  };
-  getWriters();
-}, []);
+const handleClick = () => {
+  setData((prevData) => ({
+    ...prevData,
+    loading: true,
+  }));
+  
+  setTimeout(() => {
+    const getWriters = async () => {
+      const response = await fetch("/writers.json");
+      const data = await response.json();
+      setData(data);
+    };
+    getWriters();
+  }, 2000);
+
+
+
+};
+
+if (data.loading) {
+  return (
+    <div>
+    <h1> Writer Profiles </h1>
+    <div className='container'>
+    <div className="card action">
+    <p className='infoText'> Loading...</p>
+    </div>
+    </div>
+    </div>
+  )
+}
 
 
 return (
     <div>
       <h1>Writer Profiles</h1>
      <div className="container">
-       {writers.map((writer) => (
-         <ProfileCard writer={writer} />
-       ))} 
-    
-        </div>
-    </div>
-  );
+       {data.writers.length === 0 ? (
+         <div className='card action'> 
+         <p className='infoText'> Oops... no writer profile found</p>
+         <button className='actionBtn' onClick={handleClick}>
+           Get Writers
+         </button>
+         </div>
+       ) : (
+         data.writers.map((writer) => (
+         <ProfileCard key={writer.id} writer={writer} />
+       )) 
+       )};
+       </div>
+       </div>
+);
 }
 
 export default App;
